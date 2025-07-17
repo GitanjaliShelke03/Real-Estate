ad4f9c06e3423cca71755c956c8baf27124dadf8
GitanjaliShelke03 <gitshelke@gmail.com>
2025-07-17 19:05:58 +0530

    Projects added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,7 @@ import assets from '../assets/assets';
 const About = () => {
     return (
         <div className='flex flex-col items-center justify-center container mx-auto pt-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About">
-            <h1 className='text 2xl sm:text-4xl font-bold mb-2'>About
-                <span className='underline underline-offset-4 decoration-1 under font-light'>Our
+            <h1 className='text 2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our
                     Brand</span></h1>
             <p className='text-grey-600 text-center mb-8'>
                 Passionate about Properties,Dedicated to Your Vision.
